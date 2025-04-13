4f12462cc717ac23c9257889d601d055a2687493
Enekocm <enekocanedo@gmail.com>
2025-04-13 17:02:14 +0200

    feat: Implement initial setup and master password creation
    This commit introduces the initial setup for the application, including:
    
    -   Removed the old `activity_main.xml` layout.
    -   Added a new home screen (`screen_home.xml`) with the SecureVault logo and a "Next" button.
    -   Created the Register activity (`RegisterActivity.kt`) and the associated master password creation layout (`activity_master_password.xml`).
    -   Implemented the basic navigation from the home screen to the Register activity.
    -   Added new fonts: roboto, roboto_black, roboto_bold, roboto_medium and added the file font_certs.xml.
    -   Updated the `AndroidManifest.xml` to include the new `RegisterActivity` and main activity.

---
+++ app/src/main/java/com/example/securevault/activities/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.securevault.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.securevault.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.screen_home)
+
+        findViewById<Button>(R.id.registerButton).setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/securevault/activities/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.securevault.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.securevault.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_master_password)
+    }
+}
