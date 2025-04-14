8c5c0a0ca9e3d3357b69dabb1c7be75afabe0d9e
Enekocm <enekocanedo@gmail.com>
2025-04-14 20:43:52 +0200

    refactor: Update MainActivity to use View Binding

--- app/src/main/java/com/example/securevault/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/securevault/ui/view/MainActivity.kt
@@ -2,21 +2,21 @@ package com.example.securevault.ui.view
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.example.securevault.R
+import com.example.securevault.databinding.ScreenHomeBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ScreenHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.screen_home)
+        binding = ScreenHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        findViewById<Button>(R.id.registerButton).setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
+        binding.registerButton.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
         }
-
     }
 }
