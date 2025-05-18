cdbd3fd91d5ea62aa1f141345403564cad6c555e
eneko <enekocanedo@gmail.com>
2025-05-18 19:44:32 +0200

    feat: Add ChangeMasterPasswordDialog and integrate into SettingsActivity

--- app/src/main/java/com/example/securevault/ui/view/SettingsActivity.kt
+++ app/src/main/java/com/example/securevault/ui/view/SettingsActivity.kt
@@ -1,13 +1,17 @@
 package com.example.securevault.ui.view
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.securevault.databinding.SettingsActivityBinding
+import com.example.securevault.ui.view.fragments.ChangeMasterPasswordDialog
+import com.example.securevault.ui.viewmodel.SettingsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SettingsActivity : AppCompatActivity(){
     private lateinit var binding: SettingsActivityBinding
+    private val viewModel: SettingsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +24,14 @@ class SettingsActivity : AppCompatActivity(){
 
         binding.toolbarSettings.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
+        }
+        setListeners()
+    }
+
+    private fun setListeners() {
+        binding.btnChangeMasterPassword.setOnClickListener {
+            val dialog = ChangeMasterPasswordDialog()
+            dialog.show(supportFragmentManager, "ChangeMasterPasswordDialog")
         }
     }
 }
---
+++ app/src/main/java/com/example/securevault/ui/view/fragments/ChangeMasterPasswordDialog.kt
@@ -0,0 +1,98 @@
+package com.example.securevault.ui.view.fragments
+
+import android.app.Dialog
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.graphics.drawable.toDrawable
+import androidx.core.widget.doAfterTextChanged
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import com.example.securevault.R
+import com.example.securevault.databinding.CreateMasterPasswordBinding
+import com.example.securevault.domain.model.PasswordStrength
+import com.example.securevault.ui.viewmodel.RegisterViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ChangeMasterPasswordDialog: DialogFragment() {
+    private lateinit var binding: CreateMasterPasswordBinding
+
+    private val viewModel: RegisterViewModel by viewModels()
+
+    private val label = "Strength:"
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        binding = CreateMasterPasswordBinding.inflate(layoutInflater)
+
+        binding.root.background = androidx.core.content.ContextCompat.getDrawable(requireContext(), R.drawable.dialog_background)
+
+        val newMarginTopInDp = 10
+        val layoutParams = binding.titleText.layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.topMargin = (newMarginTopInDp * resources.displayMetrics.density).toInt()
+        binding.titleText.layoutParams = layoutParams
+
+        binding.titleText.text = getString(R.string.change_master_password)
+        val displayMetrics = resources.displayMetrics
+        val width = (displayMetrics.widthPixels * 0.9).toInt()
+
+        val dialog = super.onCreateDialog(savedInstanceState).apply {
+            setContentView(binding.root)
+            setCancelable(false)
+            setCanceledOnTouchOutside(false)
+            window?.setLayout(
+                width,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            window?.setBackgroundDrawable(Color.TRANSPARENT.toDrawable())
+        }
+
+        setObserver()
+        setListeners()
+        setAfterTextChanged()
+        return dialog
+    }
+
+    private fun changeProgressBar(strength: PasswordStrength) {
+        val text = label + " " + strength.label
+        binding.passwordStrengthLabel.text = text
+        binding.passwordStrengthBar.progress = strength.progress
+        binding.passwordStrengthBar.progressTintList = ColorStateList.valueOf(strength.colorInt)
+    }
+
+    private fun setObserver() {
+        viewModel.passwordStrength.observe(this) { strength ->
+            changeProgressBar(strength)
+        }
+    }
+
+    private fun setListeners() {
+        binding.continueButton.setOnClickListener {
+            val masterPassword = binding.masterPasswordInput.text.toString()
+            val confirmPassword = binding.confirmPasswordInput.text.toString()
+            when {
+                masterPassword.isEmpty() -> {
+                    Toast.makeText(requireContext(), "Password cannot be empty", Toast.LENGTH_LONG).show()
+                }
+
+                masterPassword != confirmPassword -> {
+                    Toast.makeText(requireContext(), "The passwords must be the same", Toast.LENGTH_LONG).show()
+                }
+
+                else -> {
+                    viewModel.createAppKey(masterPassword)
+                    if (viewModel.isKeyConfigured()) {
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+    private fun setAfterTextChanged() {
+        binding.masterPasswordInput.doAfterTextChanged {
+            viewModel.calculateStrength(binding.masterPasswordInput.text.toString())
+        }
+    }
+}
