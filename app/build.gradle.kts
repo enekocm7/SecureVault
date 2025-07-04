b6d5d75a036ec5991f21dcf7efe862c8d6164bb5
Enekocm <enekocanedo@gmail.com>
2025-07-04 18:12:48 +0200

    feat: Update version code to 6 and add AD_ID permission in AndroidManifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.enekocm.securevault"
         minSdk = 30
         targetSdk = 36
-        versionCode = 5
+        versionCode = 6
         versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
